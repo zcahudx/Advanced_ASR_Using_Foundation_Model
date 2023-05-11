e078b77bf0cd462ba4b0cb2c2e3287d56dcbd3d2
Bryan <56035353+zcahudx@users.noreply.github.com>
2023-05-11 02:25:06 +0100

    Update models.py

--- models.py
+++ models.py
@@ -22,27 +22,11 @@ class Wav2Vec2CTC(nn.Module):
         super().__init__()
         self.model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base")
         self.model.freeze_feature_encoder()
-        # self.proj = nn.Linear(768, out_dims)
-        # self.weights = nn.Parameter(torch.ones(12))
-        # self.proj_1 = nn.Linear(768, 384)
-        # self.activation = nn.ReLU()
-        # self.proj_2 = nn.Linear(384, out_dims)
-        self.lstm = nn.LSTM(768, 128, 1, bidirectional=True)
-        self.proj = nn.Linear(128 * 2, out_dims)
+        self.proj = nn.Linear(768, out_dim)
 
     def forward(self, feat):
         hidden = self.model(feat)
-        # all_hidden = hidden.hidden_states[1:]
-        # intermediate = torch.stack(all_hidden, dim=-1)
-        # intermediate = torch.mul(intermediate, torch.nn.functional.softmax(self.weights)).sum(dim=-1)
-        # layer_10 = all_hidden[10]
-        # output = self.proj(hidden.last_hidden_state)
-        # output = self.proj(layer_10)
-        # output = self.proj_1(hidden.last_hidden_state)
-        # output = self.activation(output)
-        # output = self.proj_2(output)
-        output,_ = self.lstm(hidden.last_hidden_state)
-        output = self.proj(output)
+        output = self.proj(hidden.last_hidden_state)
         return output
 
 class WavLMCTC(nn.Module):
